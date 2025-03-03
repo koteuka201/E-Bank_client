4c2498fdd595a97aa6208a07a63a0345b00b5112
Viktor Kuzubov <kuzubovvv@gmail.com>
2025-03-04 00:08:48 +0700

    fixes

--- src/pages/accounts/myAccounts/page/blocks/list/index.tsx
+++ src/pages/accounts/myAccounts/page/blocks/list/index.tsx
@@ -4,13 +4,13 @@ export const List=()=>{
   
   const {data, isError}=useGetMyAccounts({userId: ''})
 
-  if(isError){
+  if(!isError){
     return(
-      <div className="">Не удалось загрузить ваши счета. Перезагрузите страницу или попробуйте позже!</div>
+      <div className="text-center font-semibold text-lg mt-10">Не удалось загрузить ваши счета. Перезагрузите страницу или попробуйте позже!</div>
     )
   }
   
   return(
-    <>{data}</>
+    <>{data?.creditCardBankAccounts}</>
   )
 }
--- src/pages/accounts/myAccounts/page/index.tsx
+++ src/pages/accounts/myAccounts/page/index.tsx
@@ -6,7 +6,7 @@ export const MyAccounts=()=>{
   return(
     <>
       <Container fluid>
-        <h2 className="font-semibold mt-0">Кошелёк</h2>
+        <h2 className="font-semibold mt-0 border-b">Кошелёк</h2>
         <Suspense fallback={<Spinner />}>
           <List />
         </Suspense>
