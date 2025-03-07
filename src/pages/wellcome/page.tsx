885c10b91524b4470546abeca5801560ad6d69ac
Viktor Kuzubov <kuzubovvv@gmail.com>
2025-03-08 00:59:30 +0700

    fixes

--- src/pages/wellcome/page.tsx
+++ src/pages/wellcome/page.tsx
@@ -1,6 +1,6 @@
 export const WellcomePage = () => {
   return (
-    <div className="flex flex-col items-center  h-screen p-4">
+    <div className="flex flex-col items-center p-4">
       <div className="text-center bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
         <div className="text-3xl font-bold text-main mb-4">Добро пожаловать в E-Bank!</div>
         <div className="text-lg text-gray-700">
