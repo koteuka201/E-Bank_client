c95e37a27e128acc75821e69471ab672b00ac5b0
Viktor Kuzubov <kuzubovvv@gmail.com>
2025-03-07 14:24:23 +0700

    add credit api

---
+++ src/shared/api/base/creditApi/api.ts
@@ -0,0 +1,14 @@
+import { CREDIT_API_BASE_URL } from "@shared/config";
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: CREDIT_API_BASE_URL,
+})
+
+api.interceptors.request.use((config) => {
+  const authToken=localStorage.getItem('token')
+  if (authToken) {
+    config.headers.Authorization = `Bearer ${authToken}`
+  }
+  return config
+})
---
+++ src/shared/api/base/creditApi/useApi.ts
@@ -0,0 +1,46 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import { api } from "./api"
+import { MutationOptions } from "../mutationOptionsType"
+import { ApiTagsEnum } from "@shared/api"
+import { AxiosRequestConfig } from "axios"
+import qs from 'qs'
+
+
+export const useCreditApiQuery = <R>(key: ApiTagsEnum[], url: string, params?: Record<string, any>, id?: string) => {
+  return useQuery<R>({
+    queryKey: key,
+    queryFn: async () => {
+      const fullUrl = id ? `${url}/${id}` : url
+      const response = await api.get<R>(fullUrl, { params,
+        paramsSerializer: params => qs.stringify(params, { arrayFormat: 'repeat' }) 
+       })
+      return response.data
+    },
+  })
+}
+
+export const useCreditApiMutation = <TData = unknown, TResponse = any>(
+  { url, method, invalidateTags, params, id, urlAfter }: MutationOptions
+) => {
+  const queryClient = useQueryClient()
+
+  const fullUrl = id ? (urlAfter ? `${url}/${id}/${urlAfter}` : `${url}/${id}`) : url
+  return useMutation<TResponse, Error, { data: TData; headers?: any; params?: Record<string, any> }>({
+    mutationFn: async ({ data, headers, params: mutationParams  }) => {
+      const config: AxiosRequestConfig<TData> = {
+        url: fullUrl,
+        method,
+        data,
+        headers,
+        params: {...params, ...mutationParams},
+      }
+      const response = await api(config)
+      return response.data
+    },
+    onSuccess: () => {
+      if (invalidateTags?.length) {
+        invalidateTags.forEach(tag => queryClient.invalidateQueries({ queryKey: [tag] }))
+      }
+    }
+  })
+}
