45d24a1d44eda421b5623b5b37202e3b947e80ee
Viktor Kuzubov <kuzubovvv@gmail.com>
2025-03-23 17:29:35 +0700

    fixes

--- src/pages/wellcome/page.tsx
+++ src/pages/wellcome/page.tsx
@@ -1,14 +1,15 @@
 import { ThemeToggle } from "@features/themeToggle";
+import { Card } from "@shared/components";
 
 export const WellcomePage = () => {
   return (
     <div className="flex flex-col items-center p-4">
-      <div className="text-center bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+      <Card className="text-center p-8 rounded-lg shadow-lg w-full max-w-md">
         <div className="text-3xl font-bold text-main mb-4">Добро пожаловать в E-Bank!</div>
-        <div className="text-lg text-gray-700">
+        <div className="text-lg text-gray-700 dark:text-white">
           Ваши финансы под контролем. Мы предлагаем быстрые и удобные решения для управления вашими деньгами.
         </div>
-      </div>
+      </Card>
       <ThemeToggle />
     </div>
   );
