e00db7b9d6cb265177c5aa934d23f7c747ed970d
Viktor Kuzubov <kuzubovvv@gmail.com>
2025-03-28 18:08:11 +0700

    add split vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,41 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias:{
-      '@shared': path.resolve(__dirname, 'src/shared'),
-      '@widgets': path.resolve(__dirname, 'src/widgets'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@features': path.resolve(__dirname, 'src/features'),
-      '@entities': path.resolve(__dirname, 'src/entities'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  console.log(env['VITE_APP_TYPE']) 
+
+  return {
+    plugins: [react()],
+    define: {
+      __APP_TYPE__: JSON.stringify(env['VITE_APP_TYPE'])
+    },
+    server: {
+      port: env['VITE_APP_TYPE'] === "client" ? 5175 : 5174
+    },
+    resolve: {
+      alias:{
+        '@shared': path.resolve(__dirname, 'src/shared'),
+        '@widgets': path.resolve(__dirname, 'src/widgets'),
+        '@pages': path.resolve(__dirname, 'src/pages'),
+        '@features': path.resolve(__dirname, 'src/features'),
+        '@entities': path.resolve(__dirname, 'src/entities'),
+      }
     }
   }
 })
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias:{
+//       '@shared': path.resolve(__dirname, 'src/shared'),
+//       '@widgets': path.resolve(__dirname, 'src/widgets'),
+//       '@pages': path.resolve(__dirname, 'src/pages'),
+//       '@features': path.resolve(__dirname, 'src/features'),
+//       '@entities': path.resolve(__dirname, 'src/entities'),
+//     }
+//   }
+// })
