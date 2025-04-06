5e99210e61aeee7e8b60408f13c232e9d8484d7f
root <root@compute-vm-2-2-15-ssd-1741252531609.ru-central1.internal>
2025-04-06 07:54:22 +0000

    userManagerSettings

--- src/shared/contexts/oauth/userManager.tsx
+++ src/shared/contexts/oauth/userManager.tsx
@@ -1,19 +1,18 @@
 /* eslint-disable @typescript-eslint/camelcase */
 import { createUserManager } from 'redux-oidc';
 import { UserManagerSettings } from 'oidc-client';
-import { UserRole } from '@shared/api';
 
 //конфигурация по которой создается openIdConnection, тут ничего не меняется
 const userManagerConfig: UserManagerSettings = {
   client_id: 'ebankclient',
-  redirect_uri: import.meta.env["VITE_APP_TYPE"]===UserRole.Client ? 'http://localhost:5175/signin-oidc' : 'http://localhost:5174/signin-oidc',//ведёт к CallbackPage(Redirect/)
+  redirect_uri: 'http://158.160.18.15:5173/signin-oidc',//ведёт к CallbackPage(Redirect/)
   response_type: 'token id_token',
   scope: 'openid bankapi',
-  authority: 'https://localhost:5001',
+  authority: 'http://158.160.18.15:5004/',
   filterProtocolClaims: true,
   loadUserInfo: true,
   monitorSession: true,
-  post_logout_redirect_uri: 'http://localhost:5173/signout-oidc'
+  post_logout_redirect_uri: 'http://158.160.18.15:5173/signout-oidc'
 };
 
 const userManager = createUserManager(userManagerConfig);
